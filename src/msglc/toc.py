a34648397f9fe6c446b461037ca9469fcb6521a9
Theodore Chang <tlcfem@gmail.com>
2024-03-09 14:23:33 +0100

    Use structured table of contents

--- src/msglc/toc.py
+++ src/msglc/toc.py
@@ -15,11 +15,19 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass, asdict
 from io import BytesIO
 
 from msgpack import Packer
 
 from .config import config
+
+
+@dataclass()
+class Node:
+    t: dict | list | None
+    p: dict | list | None
+    s: bool = False
 
 
 class TOC:
@@ -37,17 +45,21 @@ class TOC:
     def _pos(self) -> int:
         return self._buffer.tell() - self._initial_pos
 
-    def _pack(self, obj) -> dict:
+    def _pack(self, obj) -> Node:
         def _pack_bin(_obj: bytes) -> None:
             self._buffer.write(_obj)
 
         def _pack_obj(_obj) -> None:
             self._buffer.write(self._packer.pack(_obj))
 
+        def _generate(_start: int) -> Node:
+            _end = self._pos
+            return Node(None, [_start, _end], _end <= _start + config.trivial_size)
+
         if not isinstance(obj, (dict, list, set, tuple)):
             start_pos = self._pos
             _pack_obj(obj)
-            return {"p": [start_pos, self._pos]}
+            return _generate(start_pos)
 
         if isinstance(obj, tuple):
             obj = list(obj)
@@ -55,15 +67,6 @@ class TOC:
             obj = sorted(obj)
 
         start_pos = self._pos
-
-        def _simple_toc(_v) -> bool:
-            return (
-                "t" not in _v
-                and 2 == len(_v["p"])
-                and isinstance(_v["p"][0], int)
-                and isinstance(_v["p"][1], int)
-                and _v["p"][1] < _v["p"][0] + config.trivial_size
-            )
 
         obj_toc: dict | list
         all_small_obj: bool
@@ -73,38 +76,41 @@ class TOC:
             for k, v in self._transform(obj.items()):
                 _pack_obj(k)
                 obj_toc[k] = self._pack(v)
-            all_small_obj = all(_simple_toc(v) for v in obj_toc.values())
+            all_small_obj = all(v.s for v in obj_toc.values())
         elif isinstance(obj, list):
             _pack_bin(self._packer.pack_array_header(len(obj)))
             obj_toc = [self._pack(v) for v in self._transform(obj)]
-            all_small_obj = all(_simple_toc(v) for v in obj_toc)
+            all_small_obj = all(v.s for v in obj_toc)
         else:
             raise ValueError(f"Expecting dict or list, got {obj.__class__}.")
 
         if self._pos < start_pos + config.small_obj_optimization_threshold:
-            return {"p": [start_pos, self._pos]}
+            return _generate(start_pos)
 
         if all_small_obj:
             if isinstance(obj, dict) or 0 == len(obj):
-                return {"p": [start_pos, self._pos]}
+                return _generate(start_pos)
 
             groups: list = []
             accu_list: list = []
             accu_size: int = 0
             for v in obj_toc:
                 accu_list.append(v)
-                accu_size += v["p"][1] - v["p"][0]
+                accu_size += v.p[1] - v.p[0]
                 if accu_size > config.small_obj_optimization_threshold:
-                    groups.append((len(accu_list), accu_list[0]["p"][0], accu_list[-1]["p"][1]))
+                    groups.append((len(accu_list), accu_list[0].p[0], accu_list[-1].p[1]))
                     accu_list = []
                     accu_size = 0
 
             if accu_list:
-                groups.append((len(accu_list), accu_list[0]["p"][0], accu_list[-1]["p"][1]))
+                groups.append((len(accu_list), accu_list[0].p[0], accu_list[-1].p[1]))
 
-            return {"p": groups} if len(groups) > 1 else {"p": [start_pos, self._pos]}
+            return Node(None, groups) if len(groups) > 1 else _generate(start_pos)
 
-        return {"t": obj_toc, "p": [start_pos, self._pos]}
+        return Node(obj_toc, [start_pos, self._pos])
 
     def pack(self, obj) -> dict:
-        return self._pack(obj)
+        def _factory(_obj) -> dict:
+            return {k: v for k, v in _obj if v and k != "s"}
+
+        return asdict(self._pack(obj), dict_factory=_factory)
