ef2ced2dea09b5819cb8539652dc3b08a1548c44
Theodore Chang <tlcfem@gmail.com>
2026-01-01 19:03:22 +0100

    Minor update

--- src/msglc/writer.py
+++ src/msglc/writer.py
@@ -266,7 +266,7 @@ class LazyCombiner:
         if isinstance(self._buffer_or_path, str):
             _upsert(self._buffer, self._buffer_or_path, self._fs)
 
-        if isinstance(self._buffer_or_path, str | UPath):
+        if isinstance(self._buffer_or_path, (str, UPath)):
             self._buffer.close()
 
     def write(self, obj: Generator, name: str | None = None) -> None:
